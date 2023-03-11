a74afcc1f2667cfc9e9fe3cc9519b8f7fb816fcb
Mauro Colucci <colucci.mauro@gmail.com>
2023-03-11 19:35:51 -0300

    navbar user dropdown

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => window.removeEventListener("scroll", isActive);
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Jhonny Ramone",
+    isSeller: true,
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <span>fiverr</span>
@@ -13,16 +32,42 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://loff.it/wp-content/uploads/2015/10/loffit-johnny-ramone-un-bronco-icono-del-punk-02-600x450-1538943544.jpg"
+                alt="User Portrait."
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
-      <hr />
-      <div className="menu">
-        <span>tesxt</span>
-        <span>tesxt</span>
-        <span>tesxt</span>
-      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>tesxt</span>
+            <span>tesxt</span>
+            <span>tesxt</span>
+          </div>
+        </>
+      )}
     </nav>
   );
 };
